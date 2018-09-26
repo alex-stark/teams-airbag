09b68e98ee48251c9ac78bd4e9d606456bfee929
Alex Stark <alex.stark@cerner.com>
2018-09-25 22:37:17 -0500

    using better -a flag for opening the application

--- teams-airbag.sh
+++ teams-airbag.sh
@@ -3,5 +3,6 @@
 teams_pid_cnt=$(ps -ef | grep  Teams.app | grep -v grep | wc -l)
 if [ $teams_pid_cnt = 0 ]
 then
-  open /Applications/Microsoft\ Teams.app
+  # Open Teams app if not running.
+  open -a /Applications/Microsoft\ Teams.app
 fi
